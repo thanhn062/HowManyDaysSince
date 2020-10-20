6119cfc55f698c77e53c261bd649742d34c400bf
Thanh Ngo <510thanh.ngo@gmail.com>
2020-10-20 16:45:11 -0700

    Add add menu field validation

--- public/script.js
+++ public/script.js
@@ -7,10 +7,21 @@ $(document).ready(function() {
 	$(".btn-cancel").click(function() {
 		$(".reminder-list").show();
 		$(".add-menu").hide();
+		$(".alert-warning").hide();
 	})
 	$(".btn-add").click(function() {
-		
-		$(".reminder-list").show();
-		$(".add-menu").hide();
+		var name = $("#reminder-name").val();
+		var day = $("#reminder-day").val();
+		var alert = $("#reminder-alert").val();
+
+		if(name && day) {
+			$(".reminder-list").show();
+			$(".add-menu").hide();
+			$(".alert-warning").hide();
+		}
+		else {
+			$(".alert-warning").show();
+		}
+
 	})
 });
