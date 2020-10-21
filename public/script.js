b2af3f970d683c774b758f78ed4042dba63e3a1f
Thanh Ngo <510thanh.ngo@gmail.com>
2020-10-21 03:42:57 -0700

    Add functionality to Add button

--- public/script.js
+++ public/script.js
@@ -1,11 +1,39 @@
+// Get raw text from localStorage
+var raw_reminder = localStorage.getItem("reminder");
+// Parse it into an object array
+var reminder = JSON.parse(raw_reminder);
+// Make these variable global
 $(document).ready(function() {
+	// Load existing data into reminder
+	// If reminder is null, make it into an array to avoid error at array.push
+	if(!reminder)
+		reminder = [];
+	else {
+		// Display data from localStorage
+
+		// Loop through reminder
+		for (var i in reminder) {
+			var item = `
+			<div class="row">
+				<div class="col-lg-12 reminder-item">
+					<div class="row pt-3 pb-3">
+						<div class="col-9 reminder-title">${reminder[i].name}</div>
+						<div class="col-3 reminder-count">${reminder[i].day}</div>
+					</div>
+				</div>
+			</div>`;
+			$("#reminder-list").append(item);
+	   		//console.log(reminder[i].name);
+	  	}
+	}
+
 	$(".btn-circle").click(function() {
-		$(".reminder-list").hide();
+		$("#reminder-list").hide();
 		$(".add-menu").show();
 	});
 
 	$(".btn-cancel").click(function() {
-		$(".reminder-list").show();
+		$("#reminder-list").show();
 		$(".add-menu").hide();
 		$(".alert-warning").hide();
 		// Empty fields
@@ -20,7 +48,28 @@ $(document).ready(function() {
 		var alert = $("#reminder-alert").val();
 
 		if(name && day) {
-			$(".reminder-list").show();
+			// Empty fields
+			$("#reminder-name").val("");
+			$("#reminder-day").val("");
+			$("#reminder-alert").val("");
+			var obj = {"name": name, "day": day, "alert": alert};
+			// Push new reminder into the array
+			reminder.push(obj);
+			// Add into localStorage
+			localStorage.setItem("reminder", JSON.stringify(reminder));
+			// Add into reminder-list
+			var item = `
+			<div class="row">
+				<div class="col-lg-12 reminder-item">
+					<div class="row pt-3 pb-3">
+						<div class="col-9 reminder-title">${name}</div>
+						<div class="col-3 reminder-count">${day}</div>
+					</div>
+				</div>
+			</div>`;
+			$("#reminder-list").append(item);
+
+			$("#reminder-list").show();
 			$(".add-menu").hide();
 			$(".alert-warning").hide();
 		}
@@ -29,3 +78,27 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function load() {
+// If reminder is null, make it into an array to avoid error at array.push
+if(!reminder)
+	reminder = [];
+else {
+	// Display data from localStorage
+
+	// Loop through reminder
+	for (var i in reminder) {
+		var item = `
+		<div class="row">
+			<div class="col-lg-12 reminder-item">
+				<div class="row pt-3 pb-3">
+					<div class="col-9 reminder-title">${reminder[i].name}</div>
+					<div class="col-3 reminder-count">${reminder[i].day}</div>
+				</div>
+			</div>
+		</div>`;
+		$("#reminder-list").append(item);
+   		//console.log(reminder[i].name);
+  	}
+}
+}
