4ae70446b4d479d1daf4d56e8b2df179f2742fe2
Thanh Ngo <510thanh.ngo@gmail.com>
2020-10-21 23:32:02 -0700

    Added meter percentage & day difference calculation

--- public/script.js
+++ public/script.js
@@ -4,36 +4,35 @@ var raw_reminder = localStorage.getItem("reminder");
 var reminder = JSON.parse(raw_reminder);
 // Make these variable global
 $(document).ready(function() {
-	//$("#nav-reminder").toggleClass("activeNav");
-	var dayStart = "20200930";
-	var dayStart_d = dayStart.substring(6,8);
-	var dayStart_m = dayStart.substring(4,6);
-	var dayStart_y = dayStart.substring(0,4);
-
-	var given = moment(dayStart_y + "-" + dayStart_m + "-" + dayStart_d, "YYYY-MM-DD");
-	var current = moment().startOf('day');
-
-	//Difference in number of days
-	var daysAgo = Math.abs(Math.round(moment.duration(given.diff(current)).asDays()));
-	console.log(daysAgo);
-
 	// Load existing data into reminder
 	// If reminder is null, make it into an array to avoid error at array.push
 	if(!reminder)
 		reminder = [];
 	else {
 		// Display data from localStorage
-
-		// Loop through reminder
+		// First loop through reminder
 		for (var i in reminder) {
+			// Calculate days since reminder start
+			var dayStart_y = reminder[i].start.substring(0,4);
+			var dayStart_m = reminder[i].start.substring(4,6);
+			var dayStart_d = reminder[i].start.substring(6,8);
+
+			var given = moment(dayStart_y + "-" + dayStart_m + "-" + dayStart_d, "YYYY-MM-DD");
+			var current = moment().startOf('day');
+
+			var daysAgo = Math.abs(Math.round(moment.duration(given.diff(current)).asDays()));
+			// Find percentage value of current day count & maximum day count
+			var percentage = Math.round((daysAgo/reminder[i].day)*100);
+			console.log(percentage);
+			// Display data from localStorage
 			var item = `
 			<div class="row">
 				<div class="col-lg-12 reminder-item">
 					<div class="row pt-3 pb-3">
 						<div class="col-9 reminder-title">${reminder[i].name}</div>
 						<div class="col-3 reminder-count">
-							<div class="c100 p15">
-								<span>${reminder[i].day}</span>
+							<div class="c100 p${percentage}">
+								<span>${daysAgo}</span>
 								<div class="slice">
 									<div class="bar"></div>
 									<div class="fill"></div>
@@ -121,12 +120,22 @@ else {
 
 	// Loop through reminder
 	for (var i in reminder) {
+		// Calculate days since reminder start
+		var dayStart_y = reminder[i].start.substring(0,4);
+		var dayStart_m = reminder[i].start.substring(4,6);
+		var dayStart_d = reminder[i].start.substring(6,8);
+
+		var given = moment(dayStart_y + "-" + dayStart_m + "-" + dayStart_d, "YYYY-MM-DD");
+		var current = moment().startOf('day');
+
+		var daysAgo = Math.abs(Math.round(moment.duration(given.diff(current)).asDays()));
+		console.log(daysAgo);
 		var item = `
 		<div class="row">
 			<div class="col-lg-12 reminder-item">
 				<div class="row pt-3 pb-3">
 					<div class="col-9 reminder-title">${reminder[i].name}</div>
-					<div class="col-3 reminder-count">${reminder[i].day}</div>
+					<div class="col-3 reminder-count">${daysAgo}</div>
 				</div>
 			</div>
 		</div>`;
