ed5886bc886f3764a014d2669a438951eeb3d3f8
Thanh Ngo <510thanh.ngo@gmail.com>
2020-10-24 12:01:07 -0700

    Redirect to index.html

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
